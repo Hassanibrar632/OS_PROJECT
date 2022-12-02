47edf3d7eba565ebb868341feba6498f68fee449
Hassanibrar632 <hassanibrar632@gmail.com>
2022-12-03 00:31:40 +0500

    ps_working

--- xv6-riscv/user/ps.c
+++ xv6-riscv/user/ps.c
@@ -1,8 +1,15 @@
 #include "kernel/types.h"
-#include "kernel/stat.h"
+#include "kernel/param.h"
+#include "kernel/proc.h"
 #include "user/user.h"
 
+struct proc proc[NPROC];
+
 int main(){
-	printf("Hello World...\n");
+	struct proc *p;
+	printf("PID\t\tNAME\n");
+	for(p = proc; p < &proc[NPROC]; p++)
+		if(p->state == RUNNING)
+			printf("%d\t\t%s\n", p->pid, p->name);
 	return 0;
 }
--- xv6-riscv/user/ps.d
+++ xv6-riscv/user/ps.d
@@ -1,1 +1,2 @@
-user/ps.o: user/ps.c kernel/types.h kernel/stat.h user/user.h
+user/ps.o: user/ps.c kernel/types.h kernel/param.h kernel/proc.h \
+ user/user.h
